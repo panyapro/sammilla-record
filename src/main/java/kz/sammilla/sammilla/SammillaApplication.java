ed69279e1443059a962d8240190d4f7232858914
pavershin <pavel.avershin@gmail.com>
2018-12-12 20:51:14 +0600

    Create new project

---
+++ src/main/java/kz/sammilla/sammilla/SammillaApplication.java
@@ -0,0 +1,12 @@
+package kz.sammilla.sammilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SammillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SammillaApplication.class, args);
+	}
+}
---
+++ src/test/java/kz/sammilla/sammilla/SammillaApplicationTests.java
@@ -0,0 +1,16 @@
+package kz.sammilla.sammilla;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SammillaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
